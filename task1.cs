a435334ec2b1a0787ebea93db896a2c1d7727b61
Core <109795959+JustAnCore@users.noreply.github.com>
2022-12-07 14:13:42 +0300

    Solution to a 'Find the stray number'

---
+++ task1.cs
@@ -0,0 +1,30 @@
+/* 
+----------------------------------------------------------------
+https://www.codewars.com/kata/57f609022f4d534f05000024/train/csharp
+----------------------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static int Stray(int[] numbers)
+    {
+        List<int> firstGroup = new List<int> { };
+        List<int> secondGroup = new List<int> { };
+
+        for(int i = 0; i < numbers.Length; i++){
+            if(i == 0){
+                firstGroup.Add(numbers[i]);
+            }
+            else{
+                if (numbers[i] == firstGroup[0]) firstGroup.Add(numbers[i]);
+                else secondGroup.Add(numbers[i]);
+            }
+        }
+
+        if (firstGroup.Count > secondGroup.Count) return secondGroup[0];
+        else return firstGroup[0];
+    }
+}
